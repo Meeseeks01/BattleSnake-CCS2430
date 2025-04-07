c9c18e85f1ca5112fc05b231a58c191af8c378cc
Anthi Koutsouveli <akoutsouveli@athtech.gr>
2025-04-07 21:20:06 +0300

    feat: prevent wall collision BPC-6

--- src/logic/collision/boundaries.js
+++ src/logic/collision/boundaries.js
@@ -1,4 +1,22 @@
 export default function checkWallCollision(gameState, isMoveSafe) {
     //BPC-6 2 Prevent Wall Collisions in Movement Logic
+    
+   const boardWidth = gameState.board.width; //already existed
+   const boardHeight = gameState.board.height; //already existed
+
+   if (myHead.x === 0) {
+       isMoveSafe.left = false; // it prevents to go left
+   }
+   if (myHead.x === boardWidth - 1) {
+       isMoveSafe.right = false; // it prevents to go right
+   }
+   if (myHead.y === 0) {
+       isMoveSafe.down = false; // it prevents to go down
+   }
+   if (myHead.y === boardHeight - 1) {
+       // because height is equal to position 10 + 0 =11
+       isMoveSafe.up = false; // it prevents to go up
+   }
+
     return isMoveSafe;
   }
