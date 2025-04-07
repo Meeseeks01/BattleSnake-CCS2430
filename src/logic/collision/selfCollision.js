78559930856e3aea968153f77c1b2f28982da429
Masky-ib <173140869+Masky-ib@users.noreply.github.com>
2025-04-07 21:48:58 +0300

    feat: self-collision detection for snake movement BPC-7

--- src/logic/collision/selfCollision.js
+++ src/logic/collision/selfCollision.js
@@ -1,4 +1,20 @@
 export default function checkSelfCollision(gameState, isMoveSafe) {
-    //BPC-6 2 Prevent Wall Collisions in Movement Logic
-    return isMoveSafe;
+  const myBody = gameState.you.body;
+  const myHead = gameState.you.head;
+  for (let i = 0; i < myBody.length; i++) {
+    const segment = myBody[i];
+    if (myHead.x === segment.x && myHead.y + 1 === segment.y) {
+      isMoveSafe.up = false;
+    }
+    if (myHead.x === segment.x && myHead.y - 1 === segment.y) {
+      isMoveSafe.down = false;
+    }
+    if (myHead.x - 1 === segment.x && myHead.y === segment.y) {
+      isMoveSafe.left = false;
+    }
+    if (myHead.x + 1 === segment.x && myHead.y === segment.y) {
+      isMoveSafe.right = false;
+    }
   }
+  return isMoveSafe;
+}
